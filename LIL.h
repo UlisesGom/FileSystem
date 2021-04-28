7ddb56b63cabac5f1440e1e2e863bf3a00a2cdb9
George940127 <54073709+George940127@users.noreply.github.com>
2021-04-28 13:28:47 -0500

    Update LIL.h
    Comentarios agregados.

--- LIL.h
+++ LIL.h
@@ -3,13 +3,15 @@
 
 /****** Constantes y tipos de dato ******/
 
+// Este tipo de dato define una structura de una lista doblemente ligada, la cual contrendra el siguiente y anterior elemento de la lista, asi como la info del ID del inodo.
 typedef struct listitem {
     struct listitem *next;
     struct listitem *prev;
     int numeroInodo;
-	int numeroBloque;
+    int numeroBloque;
 } LISTITEM;
 
+// Este tipo de dato define a una estructura, la cual contiene el primer y ultimo elemento de la LIL.
 typedef struct {
     struct listitem *first;
     struct listitem *last;
